24b443e99046e8a59a6242bceb62f866e44d4c14
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-06-08 22:45:24 +0300

    feat: implement signup logic in auth controller

--- backend/src/controllers/auth.controller.ts
+++ backend/src/controllers/auth.controller.ts
@@ -1,3 +1,44 @@
+import { Request, Response } from 'express';
+import { prisma } from '../db/prisma.js';
+import bcryptjs from 'bcryptjs';
+import generateToken from '../utils/generateToken.js';
+
+export const signup = async (req: Request, res: Response) => {
+  try {
+    const { fullName, username, password, confirmPassword, gender } = req.body;
+    if (!fullName || !username || !password || !confirmPassword || !gender) {
+      return res.status(400).json({ error: 'All fields are required' });
+    }
+    if (password !== confirmPassword) {
+      return res.status(400).json({ error: 'Passwords don`t match' });
+    }
+    const user = await prisma.user.findUnique({ where: { username } });
+    if (user) {
+      return res.status(400).json({ error: 'Username already exists' });
+    }
+    const salt = await bcryptjs.genSalt(10);
+    const hashedPassword = await bcryptjs.hash(password, salt);
+    const boyProfilePic = `https://avatarapi.runflare.run/public/boy?username=${username}`;
+    const girlProfilePic = `https://avatarapi.runflare.run/public/girl?username=${username}`;
+    const newUser = await prisma.user.create({
+      data: {
+        fullName,
+        username,
+        password: hashedPassword,
+        gender,
+        profilePic: gender === 'male' ? boyProfilePic : girlProfilePic,
+      },
+    });
+    if (newUser) {
+      generateToken(newUser.id, res);
+      return res
+        .status(201)
+        .json({ message: 'User created successfully', user: newUser });
+    }
+    return res.status(400).json({ error: 'Invalid user data' });
+  } catch (error) {
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+};
 export const login = (req: Request, res: Response) => {};
 export const logout = (req: Request, res: Response) => {};
-export const signup = (req: Request, res: Response) => {};
