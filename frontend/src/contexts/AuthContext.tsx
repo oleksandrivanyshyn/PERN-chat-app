fda622844da8902e89f3fe0bd909d3630e864809
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-06-09 15:09:55 +0300

    feat: add AuthContext with user fetching logic and integrate it into main app

---
+++ frontend/src/contexts/AuthContext.tsx
@@ -0,0 +1,60 @@
+import {
+  type Dispatch,
+  type ReactNode,
+  type SetStateAction,
+  createContext,
+  useState,
+  useEffect,
+} from 'react';
+
+type AuthUserType = {
+  id: string;
+  fullName: string;
+  email: string;
+  profilePic: string;
+  gender: string;
+};
+
+const AuthContext = createContext<{
+  authUser: AuthUserType | null;
+  setAuthUser: Dispatch<SetStateAction<AuthUserType | null>>;
+  isLoading: boolean;
+}>({
+  authUser: null,
+  setAuthUser: () => {},
+  isLoading: true,
+});
+
+export const AuthContextProvider = ({ children }: { children: ReactNode }) => {
+  const [authUser, setAuthUser] = useState<AuthUserType | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const res = await fetch('/api/auth/me');
+        const data = await res.json();
+        if (!res.ok) {
+          throw new Error(data.error);
+        }
+        setAuthUser(data);
+      } catch (error) {
+        console.error('Error fetching user:', error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+    fetchUser();
+  });
+
+  return (
+    <AuthContext.Provider
+      value={{
+        authUser,
+        isLoading,
+        setAuthUser,
+      }}
+    >
+      {children}
+    </AuthContext.Provider>
+  );
+};
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,11 +3,14 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router';
 import './index.css';
 import App from './App.tsx';
+import { AuthContextProvider } from './contexts/AuthContext.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <BrowserRouter>
-      <App />
+      <AuthContextProvider>
+        <App />
+      </AuthContextProvider>
     </BrowserRouter>
   </StrictMode>,
 );
