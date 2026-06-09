131c629639826725ebe4b663aba179b87b2e0afe
Oleksandr Ivanyshyn <o.ivanyshyn.dev@gmail.com>
2026-06-09 18:56:55 +0300

    feat: integrate Socket.IO for real-time communication and user presence tracking

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,9 @@
 import express from 'express';
 import cookieParser from 'cookie-parser';
-const app = express();
 import authRoutes from './routes/auth.route.js';
 import messagesRoutes from './routes/message.route.js';
 import dotnev from 'dotenv';
+import { app, server } from './socket/socket.js';
 const PORT = process.env.PORT || 5000;
 dotnev.config();
 app.use(cookieParser());
@@ -11,5 +11,4 @@ app.use(express.json());
 
 app.use('/api/auth', authRoutes);
 app.use('/api/messages', messagesRoutes);
-
-app.listen(5000, () => console.log('Server started on port 5000'));
+server.listen(PORT, () => console.log('Server started on port 5000'));
